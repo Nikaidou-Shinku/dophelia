2486b9a4289d99e9d716cd2eec6491a0f5e7cc69
yurzhang <yurzhang.oi@gmail.com>
2024-07-16 18:42:58 +0800

    build: setup TanStack Query

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 /* @refresh reload */
 import { lazy } from "solid-js";
 import { render } from "solid-js/web";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { Route, Router } from "@solidjs/router";
 import "./index.css";
 
 const Home = lazy(() => import("~/pages/Home"));
 
+const queryClient = new QueryClient();
+
 render(
   () => (
-    <Router>
-      <Route path="/" component={Home} />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Route path="/" component={Home} />
+      </Router>
+    </QueryClientProvider>
   ),
   document.getElementById("root")!,
 );
