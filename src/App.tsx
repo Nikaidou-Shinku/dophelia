50745674eed69712ed3dd82be9c601934e42bfd3
yurzhang <yurzhang.oi@gmail.com>
2024-07-16 18:13:00 +0800

    build: setup Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { createSignal } from 'solid-js'
 import solidLogo from './assets/solid.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = createSignal(0)
@@ -10,14 +9,14 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
+          <img src={viteLogo} alt="Vite logo" />
         </a>
         <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
+          <img src={solidLogo} alt="Solid logo" />
         </a>
       </div>
       <h1>Vite + Solid</h1>
-      <div class="card">
+      <div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count()}
         </button>
@@ -25,7 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
+      <p class="text-xl">
         Click on the Vite and Solid logos to learn more
       </p>
     </>
